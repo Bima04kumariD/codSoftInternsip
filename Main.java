20c212864e5289031468dee8854e679ba2048c23
Bima04kumariD <ddbimakumari@gmail.com>
2025-03-09 17:39:34 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,35 @@
+import java.util.Random;
 import java.util.Scanner;
-class StudentGradeCalculator{
-    public static void main(String[] args){
-        int n;
-        float sum= 0.0F;
-        String grade;
+class RandomGame{
+    public static void gameFunction(){
+        Random ran=new Random();
+        long originalNumber=ran.nextLong(100);
+        int no=(int)originalNumber%1000;
+        int n=Math.abs(no);
+        int score=100;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of subjects:");
-        n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            System.out.println("Enter mark of subject "+i);
-            int mk=sc.nextInt();
-            sum += mk;
-        }
-        float buf=sum/(n*100);
-        float averagePercentage=buf*100;
-        if(averagePercentage>=95){
-            grade="O";
-        }
-        else if (averagePercentage>=90) {
-            grade="A+";
-        }
-        else if(averagePercentage<90 && averagePercentage>=80){
-            grade="A";
-        }
-        else if(averagePercentage<80 && averagePercentage>=70){
-            grade="B+";
-        }
-        else if(averagePercentage<70 && averagePercentage>60){
-            grade="B";
-        }
-        else{
-            grade="C";
-        }
-        System.out.println("Total Marks:"+sum);
-        System.out.println("Percentage:"+averagePercentage);
-        System.out.println("Grade:"+grade);
+        int ch=0;
+        while(ch!=n){
+            ch=sc.nextInt();
+            if(ch<n){
+                System.out.println("Your guess is smaller");
+                score=score-5;
+            }
+            else if (ch>n) {
+                System.out.println("Your guess is larger");
+                score=score-5;
+            }
+            else{
+                System.out.println("Your guess is correct");
+                score=score+10;
+            }
+
+    }
+        System.out.println("Final score ="+score);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Random Game!!! \n You have three chances to guess the number right.....\nAll the best....\nLet's get started....\nEnter your first guess:");
+        gameFunction();
     }
 }
